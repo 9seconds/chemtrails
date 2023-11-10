323eae6bf239237c405d46ce53385d96d4e29c31
9seconds <nineseconds@yandex.ru>
2023-11-10 10:34:11 +0000

    Reexport main entries to global module

---
+++ chemtrails/__init__.py
@@ -0,0 +1,9 @@
+from chemtrails.trails import Snapshot  # noqa: F401
+from chemtrails.trails import sniff  # noqa: F401
+from chemtrails.trails import Trace  # noqa: F401
+
+__all__ = (
+    "sniff",
+    "Snapshot",
+    "Trace"
+)
--- chemtrails/trails/__init__.py
+++ chemtrails/trails/__init__.py
@@ -1,3 +1,9 @@
 from chemtrails.trails.archive import sniff  # noqa: F401
 from chemtrails.trails.snapshots import Snapshot  # noqa: F401
 from chemtrails.trails.traces import Trace  # noqa: F401
+
+__all__ = (
+    "sniff",
+    "Snapshot",
+    "Trace"
+)
